ccdc3817f9fe980af58681fc7e0d842c631c9d5d
Paulin JUHEN <paulin.juhen@outlook.com>
2024-10-28 11:29:10 +0100

    Création du projet + debut sur la tool bar avec un theme custom

---
+++ src/app/tool-bar/tool-bar.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatToolbarModule } from "@angular/material/toolbar";
+
+@Component({
+  selector: 'app-tool-bar',
+  standalone: true,
+  imports: [
+    MatToolbarModule,
+    MatIconModule,
+    MatButtonModule
+  ],
+  templateUrl: './tool-bar.component.html',
+  styleUrl: './tool-bar.component.scss'
+})
+export class ToolBarComponent {
+
+}
