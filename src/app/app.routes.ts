aaf09ce8cfa4100dbe55febcde045607be2e29ba
Paulin JUHEN <paulin.juhen@outlook.com>
2024-10-31 09:39:47 +0100

    Ajout des bases pour la page de contact

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomePageComponent } from './home-page/home-page.component';
 import { BuildSensorPageComponent } from './build-sensor-page/build-sensor-page.component';
+import { ContactPageComponent } from './contact-page/contact-page.component';
 
 export const routes: Routes = [
     { path: '', component: HomePageComponent },
-    { path: 'construire-un-capteur', component: BuildSensorPageComponent }
+    { path: 'construire-un-capteur', component: BuildSensorPageComponent },
+    { path: 'nous-contacter', component: ContactPageComponent }
 ];
