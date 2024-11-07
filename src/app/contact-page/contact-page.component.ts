32732ec132ac22c263ee87d251df1fea338965f2
Paulin JUHEN <paulin.juhen@outlook.com>
2024-11-07 17:51:58 +0100

    Création du front end pour la page de contact

--- src/app/contact-page/contact-page.component.ts
+++ src/app/contact-page/contact-page.component.ts
@@ -1,12 +1,25 @@
 import { Component } from '@angular/core';
+import { ResponsiveService } from '../services/responsive.service';
+import { CommonModule } from '@angular/common';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-contact-page',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule,
+  ],
   templateUrl: './contact-page.component.html',
   styleUrl: './contact-page.component.scss'
 })
 export class ContactPageComponent {
 
+  constructor(public responsiveService: ResponsiveService) {  }
 }
