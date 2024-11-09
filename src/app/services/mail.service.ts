905c4b193952ddc177f227b8168f5500b9140a1b
Paulin JUHEN <paulin.juhen@outlook.com>
2024-11-09 16:37:12 +0100

    Ajout de Reactive Form pour le formulaire de contact

---
+++ src/app/services/mail.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MailService {
+
+
+  sendMail(name: string, firstName: string, email: string, subject: string, message: string) {
+    
+  }
+  constructor() { }
+}
