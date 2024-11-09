77c52f056e673465185bda2209bfff36f37c91f7
Paulin JUHEN <paulin.juhen@outlook.com>
2024-11-09 16:31:18 +0100

    Ajout de Reactive Form pour le formulaire de contact

--- src/app/contact-page/contact-page.component.ts
+++ src/app/contact-page/contact-page.component.ts
@@ -5,6 +5,8 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { MailService } from '../services/mail.service';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact-page',
@@ -15,11 +17,44 @@ import { MatIconModule } from '@angular/material/icon';
     MatInputModule,
     MatButtonModule,
     MatIconModule,
+    ReactiveFormsModule,
   ],
   templateUrl: './contact-page.component.html',
   styleUrl: './contact-page.component.scss'
 })
 export class ContactPageComponent {
+  contactForm = new FormGroup({
+    name: new FormControl('', [Validators.required, Validators.minLength(3)]),
+    firstName: new FormControl('', [Validators.required, Validators.minLength(3)]),
+    email: new FormControl('', [Validators.required, Validators.minLength(3), Validators.email]),
+    subject: new FormControl('', [Validators.required, Validators.minLength(3)]),
+    message: new FormControl('', [Validators.required, Validators.minLength(3)]),
+  });
 
-  constructor(public responsiveService: ResponsiveService) {  }
+  submit() {
+    console.log("Submit");
+    this.mailService.sendMail('JUHEN', 'Paulin', 'paulin.juhen@outlook.com', 'Test de l\'envoi des mails', 'J\'envoie ça pour tester');
+  }
+
+  get name() {
+    return this.contactForm.get('name');
+  }
+
+  get firstName() {
+    return this.contactForm.get('firstName');
+  }
+
+  get email() {
+    return this.contactForm.get('email');
+  }
+
+  get subject() {
+    return this.contactForm.get('subject');
+  }
+
+  get message() {
+    return this.contactForm.get('message');
+  }
+
+  constructor(public responsiveService: ResponsiveService, private mailService: MailService) { }
 }
