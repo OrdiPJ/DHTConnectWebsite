a05d796bbba0a8e701972da225ee90f5ac74a585
Paulin JUHEN <paulin.juhen@outlook.com>
2025-01-05 10:34:43 +0100

    Finition du carousel + ancrage de la toolbar lors du défilement

--- src/app/services/news.service.ts
+++ src/app/services/news.service.ts
@@ -6,12 +6,14 @@ import { AuthService } from './auth.service';
   providedIn: 'root'
 })
 export class NewsService {
+  apiUrl = "https://dht-connect-api.vercel.app/api/post";
+  //apiUrl = "http://localhost:8080/api/post";
   post(title: string | undefined | null, imageURL: string | undefined | null, content: string | undefined | null) {
-    return this.http.post<any>("http://localhost:8080/api/post", {title: title, imageURL: imageURL, content: content, token : this.auth.user?.token})
+    return this.http.post<any>(this.apiUrl, {title: title, imageURL: imageURL, content: content, token : this.auth.user?.token})
   }
 
   get(lastId?: string | "0") {
-    return this.http.get<any>(`http://localhost:8080/api/post/${lastId}`);
+    return this.http.get<any>(`${this.apiUrl}/${lastId}`);
   }
 
   constructor(private http: HttpClient, private auth: AuthService) { }
